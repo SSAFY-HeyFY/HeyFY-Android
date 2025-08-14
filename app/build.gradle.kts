82b1de2d9f93c0c2ebe64b4afca32bb58b510c19
jaehong <83493143+prk4224@users.noreply.github.com>
2025-08-14 09:37:42 +0900

    #3 app 모듈에 common 모듈 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,7 +53,7 @@ android {
 }
 
 dependencies {
-
+    implementation(project(":common"))
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
