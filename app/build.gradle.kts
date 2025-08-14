0e75d7c2a3bfdd3531ad7bdb64beaaa4ee79503c
jaehong <83493143+prk4224@users.noreply.github.com>
2025-08-14 11:26:04 +0900

    #2 app 모듈에 navigation 모듈 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,7 @@ android {
 
 dependencies {
     implementation(project(":common"))
+    implementation(project(":navigation"))
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
