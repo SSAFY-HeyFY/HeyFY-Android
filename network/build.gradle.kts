f48d50c62382640e10fe0855d439de7fe2817a1d
jaehong <83493143+prk4224@users.noreply.github.com>
2025-08-19 08:21:10 +0900

    #26 network 모듈 생성

---
+++ network/build.gradle.kts
@@ -0,0 +1,67 @@
+import java.util.Properties
+import kotlin.toString
+
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.hilt)
+    kotlin("kapt")
+}
+
+android {
+    namespace = "com.ssafy.network"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 24
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+
+            buildConfigField("String", "API_PATH", getApiPath())
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        buildConfig = true
+    }
+}
+
+dependencies {
+    implementation(project(":common"))
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.dagger.hilt)
+    kapt(libs.dagger.hilt.compiler)
+    implementation(libs.bundles.retrofit)
+
+    implementation(libs.timber)
+}
+
+fun getApiPath(): String {
+    val props = loadProperties()
+    return props["API_PATH"].toString()
+}
+
+fun loadProperties(): Properties {
+    val filePath = "local.properties"
+    val props = Properties()
+    props.load(file(rootProject.file(filePath)).inputStream())
+    return props
+}
