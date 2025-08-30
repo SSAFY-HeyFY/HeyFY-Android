c08739c95821e1f1ebcb5c6651aa2e2e991ea11a
jaehong <83493143+prk4224@users.noreply.github.com>
2025-08-30 12:20:33 +0900

    [NO-ISSUE] HomeAPI GET 에서 POST로 변경

--- data/home/src/main/java/com/ssafy/home/api/HomeApi.kt
+++ data/home/src/main/java/com/ssafy/home/api/HomeApi.kt
@@ -3,9 +3,10 @@ package com.ssafy.home.api
 import com.ssafy.home.api.response.HomeResponse
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 interface HomeApi {
 
-    @GET("/home")
+    @POST("/home")
     suspend fun fetch(): Response<HomeResponse>
 }
